21ea7a11a460eebde126678709bbceda5bc37eb6
Shivam-Dhyani <shivamdhiyani15@gmail.com>
2024-10-19 23:41:59 +0530

    Enable CORS for frontend error solved

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./config/db");
 
 // Load environment variables from .env
